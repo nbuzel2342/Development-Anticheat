c059b0428801a1dcbdecc8aba97f3d3e58673887
N.Buzel <noahvidbro12@gmail.com>
2026-02-14 18:49:42 +0100

    Add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'Development Anticheat'
+
+client_script 'client/client.lua'
+server_script 'server/server.lua'
+
+-- Add any additional files or configurations here
